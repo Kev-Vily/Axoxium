bc182714c0d87540e14591b2eae968e19348cd6f
FiveDextor <160398952+FiveDextor@users.noreply.github.com>
2024-12-12 09:13:51 +0700

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,5 +1,6 @@
-const regenWall = extend(Wall, "powered-regen-wall", {
-  super.update();
-   this.health += 0.1 * Time.delta();
-   this.health = Math.min(this.health, this.maxHealth);
+block.buildType = () => extend(Building, block, {
+  update(){
+    super.update();
+    //applies overcock or something
+  }
 });

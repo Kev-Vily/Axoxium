b911daaa763a4e0e05c01b42b5e57f9e4f7413ab
FiveDextor <160398952+FiveDextor@users.noreply.github.com>
2024-12-12 08:52:44 +0700

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,14 +1,5 @@
-const RegenWall = extends Wall {
-static powerConsumption = 0.1; // Power consumption per second
-
-  updateTile() {
-    super.updateTile();
-
-    if (this.tile.entity == this) {
-      // Consume power and regenerate health
-      if (this.tile.block().consumePower(RegenWall.powerConsumption * Time.delta())) {
-        this.health += 0.1 * Time.delta();
-        this.health = Math.min(this.health, this.maxHealth);
-      }
-    }
-  }
+const regenWall = extend(Wall, "powered-regen-wall", {
+  super.update();
+   this.health += 0.1 * Time.delta();
+   this.health = Math.min(this.health, this.maxHealth);
+});

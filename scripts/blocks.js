16127ec249688a57fa6a2151517adb607dde728f
FiveDextor <160398952+FiveDextor@users.noreply.github.com>
2024-12-12 07:47:55 +0700

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,5 +1,14 @@
-const poRegenWall = () => extend(Wall, powered-regen-wall, {
-  update(){
-    super.update();
+export class RegenWall extends Wall {
+static powerConsumption = 0.1; // Power consumption per second
+
+  updateTile() {
+    super.updateTile();
+
+    if (this.tile.entity == this) {
+      // Consume power and regenerate health
+      if (this.tile.block().consumePower(RegenWall.powerConsumption * Time.delta())) {
+        this.health += 0.1 * Time.delta();
+        this.health = Math.min(this.health, this.maxHealth);
+      }
+    }
   }
-});

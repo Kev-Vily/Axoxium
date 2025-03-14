d24692955470e0468ab5e9323b4f04fc2d5634b6
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-03-14 20:30:38 +0700

    Translate-Able Mod Info

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const axoxium = Vars.mods.locateMod("axoxium").meta;
+
+axoxium.displayName = Core.bundle.get("mod.name");
+axoxium.author = Core.bundle.get("mod.author");
+axoxium.description = Core.bundle.get("mod.description");
+axoxium.subtitle = Core.bundle.get("mod.subtitle");

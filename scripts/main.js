55302a189fa11f818f2e68e8f9b71158b0daebe3
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-03-19 09:01:06 +0700

    Fix Issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const axoxium = Vars.mods.locateMod("axoxium").meta;
 
-axoxium.displayName = Core.bundle.get("mod.name");
-axoxium.author = Core.bundle.get("mod.author");
-axoxium.description = Core.bundle.get("mod.description");
-axoxium.subtitle = Core.bundle.get("mod.subtitle");
+axoxium.displayName = Core.bundle.get("mod.axoxium.name");
+axoxium.author = Core.bundle.get("mod.axoxium.author");
+axoxium.description = Core.bundle.get("mod.axoxium.description");
+axoxium.subtitle = Core.bundle.get("mod.axoxium.subtitle");
